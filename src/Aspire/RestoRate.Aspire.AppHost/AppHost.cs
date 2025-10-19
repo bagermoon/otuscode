ae3ba2e0f082bd6f1ea33b74e77d4d2e12d05d97
bagermen <bagermen@yandex.ru>
2025-10-19 15:33:53 +0300

    clean up code

--- src/Aspire/RestoRate.Aspire.AppHost/AppHost.cs
+++ src/Aspire/RestoRate.Aspire.AppHost/AppHost.cs
@@ -8,8 +8,8 @@ using Scalar.Aspire;
 // https://www.youtube.com/watch?v=_aCuwWiKncY
 var builder = DistributedApplication.CreateBuilder(args);
 
-var rabbitUsername = builder.AddParameter("rabbitmq-username", secret: true);
-var rabbitPassword = builder.AddParameter("rabbitmq-password", secret: true);
+var rabbitUsername = builder.AddParameter("rabbitmq-username", "admin", secret: true);
+var rabbitPassword = builder.AddParameter("rabbitmq-password", "admin", secret: true);
 
 var rabbitmq = builder.AddRabbitMQ("messaging", userName: rabbitUsername, password: rabbitPassword)
     .WithImageTag("4.1")
@@ -47,18 +47,5 @@ var keycloak = builder.AddKeycloak("keycloak", 8080,
 
 keycloakUsername.WithParentRelationship(keycloak);
 keycloakPassword.WithParentRelationship(keycloak);
-
-var auth = builder.AddProject<RestoRate_AuthTest>("auth-test")
-    .WithReference(keycloak)
-    .WithReference(scalar);
-
-scalar.WithApiReference(auth, options =>
-{
-    options.AddDocument("v1", "Auth API");
-});
-
-builder.AddProject<RestoRate_BlazorUI>("blazor-ui")
-    .WithReference(scalar)
-    .WithReference(keycloak);
 
 await builder.Build().RunAsync();
