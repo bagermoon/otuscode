590948d0396baf557b4622956adc9949695a5029
bagermoon <r.kuznecov@yahoo.com>
2026-01-26 08:24:17 +0300

    feat(review): add composite event for validation completion

--- src/ReviewService/RestoRate.ReviewService.Application/Sagas/ReviewSaga/ReviewStateMachine.cs
+++ src/ReviewService/RestoRate.ReviewService.Application/Sagas/ReviewSaga/ReviewStateMachine.cs
@@ -15,6 +15,7 @@ public class ReviewStateMachine : MassTransitStateMachine<ReviewState>
     public Event<ReviewAddedEvent> ReviewAdded { get; private set; } = default!;
     public Event<RestaurantValidationCompleted> RestaurantValidationCompleted { get; private set; } = default!;
     public Event<UserValidationCompleted> UserValidationCompleted { get; private set; } = default!;
+    public Event ValidationsCompleted { get; private set; } = default!;
     public ReviewStateMachine()
     {
         InstanceState(x => x.CurrentState);
@@ -35,6 +36,12 @@ public class ReviewStateMachine : MassTransitStateMachine<ReviewState>
             x.CorrelateById(context => context.Message.ReviewId);
         });
 
+        CompositeEvent(
+            () => ValidationsCompleted,
+            x => x.ValidationCompleted,
+            RestaurantValidationCompleted,
+            UserValidationCompleted);
+
         Initially(
             When(ReviewAdded)
                 .Then(context =>
@@ -43,7 +50,7 @@ public class ReviewStateMachine : MassTransitStateMachine<ReviewState>
                     context.Saga.UserId = context.Message.AuthorId;
                     context.Saga.CreatedAt = DateTime.UtcNow;
                 })
-                .Publish(async context => new ReviewValidationRequested(
+                .Publish(context => new ReviewValidationRequested(
                     RestaurantId: context.Saga.RestaurantId,
                     ReviewId: context.Saga.CorrelationId,
                     UserId: context.Saga.UserId))
@@ -56,27 +63,30 @@ public class ReviewStateMachine : MassTransitStateMachine<ReviewState>
                 {
                     context.Saga.RestaurantValidatedAt = context.Message.ValidatedAt;
                     context.Saga.IsRestaurantValid = context.Message.IsValid;
-                })
-                .If(context => 
-                    context.Saga.IsRestaurantValid == true
-                    && context.Saga?.IsUserValid == true,
-                    thenBinder => thenBinder.TransitionTo(ValidationOk))
-            ,
+                }),
             When(UserValidationCompleted)
                 .Then(context =>
                 {
                     context.Saga.UserValidatedAt = context.Message.ValidatedAt;
                     context.Saga.IsUserValid = context.Message.IsValid;
-                })
-                .If(context =>
-                    context.Saga.IsRestaurantValid == true
-                    && context.Saga?.IsUserValid == true,
-                    thenBinder => thenBinder.TransitionTo(ValidationOk))
+                }),
+            When(ValidationsCompleted)
+                .IfElse(
+                    context => context.Saga.IsRestaurantValid == true && context.Saga.IsUserValid == true,
+                    thenBinder => thenBinder.TransitionTo(ValidationOk),
+                    elseBinder => elseBinder.TransitionTo(ValidationFailed))
         );
 
         During(ValidationOk,
             Ignore(ReviewAdded),
             Ignore(RestaurantValidationCompleted),
-            Ignore(UserValidationCompleted));
+            Ignore(UserValidationCompleted),
+            Ignore(ValidationsCompleted));
+
+        During(ValidationFailed,
+            Ignore(ReviewAdded),
+            Ignore(RestaurantValidationCompleted),
+            Ignore(UserValidationCompleted),
+            Ignore(ValidationsCompleted));
     }
 }
